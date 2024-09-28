ac4bc58a8cdbb860658049f8f9bcb52f3b92b333
Sanjay Bhat <sanjaygbhat@gmail.com>
2024-09-28 10:50:15 +0530

    GPT suggestions

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,9 @@
-from .cuda_init import init_cuda
-init_cuda()
-
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_jwt_extended import JWTManager
 from config import Config
+import os
 
 db = SQLAlchemy()
 migrate = Migrate()
