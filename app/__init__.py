8178e5c740e5431c501818060dbb96986f8804bb
Sanjay Bhat <sanjaygbhat@gmail.com>
2024-09-28 08:46:54 +0530

    Fixed Flask DB

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,12 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 from flask_jwt_extended import JWTManager
 from config import Config
 import os
 
 db = SQLAlchemy()
+migrate = Migrate()
 jwt = JWTManager()
 
 def create_app(config_class=Config):
@@ -12,6 +14,7 @@ def create_app(config_class=Config):
     app.config.from_object(config_class)
 
     db.init_app(app)
+    migrate.init_app(app, db)
     jwt.init_app(app)
 
     # Import models
