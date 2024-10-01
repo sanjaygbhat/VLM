0895a467c9bcb6f22eba131825a8db14ccf45ae2
Sanjay Bhat <sanjaygbhat@gmail.com>
2024-10-01 16:17:46 +0530

    Used pretrained image processor

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 import torch
-from app.utils.image_processor import ImageProcessor
+from transformers import AutoImageProcessor
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
@@ -12,12 +12,10 @@ class Config:
     SQLALCHEMY_DATABASE_URI = DATABASE_URL
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-    
     # File Storage Configuration
     UPLOAD_FOLDER = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'uploads')
     INDEX_FOLDER = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'indices')
     DOCUMENT_INDEX_FILE = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'document_indices.json')
     DEVICE = device
-    IMAGE_PROCESSOR = ImageProcessor(device)
-    
+    IMAGE_PROCESSOR = AutoImageProcessor.from_pretrained("openbmb/MiniCPM-V-2_6", trust_remote_code=True)
 
